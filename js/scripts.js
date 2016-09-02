db2983f7003839959bcdd575f7c96d7aa3179aa5
Francis Prus <francis.prus@hotmail.com>
2016-09-02 10:55:59 -0700

    Add variables to the ui.

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,19 @@ function Pizza(pizzaSize, pizzaTopping) {
 }
 
 Pizza.prototype.price = function () {
-  
-}
+  var base = 5;
+
+  if(this.pizzaSize === "small") {
+    base = base + (this.pizzaTopping.length * 2);
+  } else if (this.pizzaSize === "medium") {
+    base += 5 + (this.pizzaTopping.length * 2);
+  } else if (this.pizzaSize === "large") {
+    base += 7 + (this.pizzaTopping.length * 2);
+  };
+  console.log(base);
+  return base;
+};
+
 
 
 
